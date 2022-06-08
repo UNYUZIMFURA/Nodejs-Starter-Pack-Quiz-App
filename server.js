78948bc147b97c9b670dd2831c76f0f1bf0f00ce
FullStackKevin <joykevinrobin1@gmail.com>
2022-06-08 15:50:16 +0200

    Changed

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express')
 const dotenv = require('dotenv')
 const logger = require('./middleware/logger')
 const morgan = require('morgan')
-// ROute files
+const connectDB = require('./config/db')
+const colors = require('colors')
 
-const bootcamps = require('./routes/bootcamps')
+
 //Load env vars
 
 dotenv.config({path: './config/config.env'})
+
+
+// Connect to database
+
+connectDB()
+// Route files
+const bootcamps = require('./routes/bootcamps')
 
 const app = express()
 
@@ -24,6 +32,15 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () =>{
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+const server = app.listen(PORT, () =>{
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+})
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red)
+
+    //close server & exit process
+
+    server.close(() => process.exit(1))
 })
